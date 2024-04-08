42e4a259aeff015d4f4a3e26c5c0fd7ccc8b332c
Stykes1 <chagoren@gmail.com>
2024-04-08 13:49:35 -0600

    Added dummy data to insert.sql

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -8,4 +8,23 @@ INSERT INTO userTable (username, password, first_name, last_name, email, birth_d
 
 INSERT INTO events (game_id, team_f, team_n, odds_f, odds_n, outcome_f)
     VALUES
-    ('adcea12351d134ar1', 'Indiana St Sycamores', 'Utah Utes', )
+    ('adcea12351d134ar1', 'Indiana St Sycamores', 'Utah Utes', -102, -118, TRUE),
+    ('acegae12354ce3a12', 'Chicago White Sox', 'Cleveland Guardians', -110, -110, FALSE),
+    ('adcf31c235a231ad1', 'Miami Marlins', 'New York Yankees', -155,130, TRUE);
+
+INSERT INTO bets (game_id, bet_value, winnging, deal_id)
+    VALUES
+    ('adcea12351d134ar1', 123.2, 4123, 1),
+    ('acegae12354ce3a12', 1223, 4223, 1),
+    ('adcf31c235a231ad1', 1251, 4141, 1);
+
+INSERT INTO userHistory (user_id, bet_id)
+    VALUES
+    (1,1),
+    (1,2),
+    (2,3);
+
+INSERT INTO deals (type, amount)
+    VALUES
+    ('Test', 3412),
+    ('Test 2', 412);
