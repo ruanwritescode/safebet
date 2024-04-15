47c586f4070269708003878e95bceba19600798f
rohanadepu <45407301+rohanadepu@users.noreply.github.com>
2024-04-15 15:50:32 -0600

    Update script.js
    fixed bug in script.js

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -2,12 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         team: [
-            { name: 'Ruan Abarbanel', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
-            { name: 'Rohan Adepu', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
-            { name: 'Joshua Gildred', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
-            { name: 'Chasen Goren', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
-            { name: 'Robiel Kennedy', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
-            { name: 'Aaron Van', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+            { name: 'Ruan Abarbanel', image: '', description: 'Software Engineer' },
+            { name: 'Rohan Adepu', image: '', description: 'Software Engineer' },
+            { name: 'Joshua Gildred', image: '', description: 'Software Engineer' },
+            { name: 'Chasen Goren', image: '', description: 'Software Engineer' },
+            { name: 'Robiel Kennedy', image: '', description: 'Software Engineer' },
+            { name: 'Aaron Van', image: '', description: 'Software Engineer' },
         ]
     }
 });
