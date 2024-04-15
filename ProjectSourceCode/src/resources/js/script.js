0129e4d2c12f14a4b1aae4ea16cfdda79a426e5d
rohanadepu <45407301+rohanadepu@users.noreply.github.com>
2024-04-15 15:13:19 -0600

    updated styling for about.hbs
    modified about.hbs file to include new styling updates. Changed the style.css and script.js file in order to support the new styling changes

---
+++ ProjectSourceCode/src/resources/js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        team: [
+            { name: 'Ruan Abarbanel', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+            { name: 'Rohan Adepu', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+            { name: 'Joshua Gildred', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+            { name: 'Chasen Goren', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+            { name: 'Robiel Kennedy', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+            { name: 'Aaron Van', image: 'https://via.placeholder.com/150', description: 'Software Engineer' },
+        ]
+    }
+});
+
